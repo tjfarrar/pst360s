340a9b8f6f0fbf40fe1cc7733fa2fc970ae18b8d
Thomas Joel Farrar <55871604+tjfarrar@users.noreply.github.com>
2024-09-19 06:24:02 +0100

    Add files via upload

---
+++ PST360S_Test1_Surname_StudentNr.R
@@ -0,0 +1,46 @@
+# PST360S Test 1 Answer Script
+# Name: 
+# Student Number: 
+
+# Make sure you have installed the necessary packages:
+install.packages(c("tidyverse", "summarytools", "readxl"),
+                 dependencies = TRUE)
+
+# Answer each question under the corresponding code.
+# ONLY YOUR CODE WRITTEN IN THIS FILE WILL BE MARKED.
+
+# 1 a)
+
+
+# 1 b)
+# Note: In case you were unable to do 1 a) correctly, 
+#  you can run the following line to create the correct dat object
+#  (this will not earn you any marks for 1 a))
+load("test1objects.RData")
+
+# 1 c)
+# Note: In case you were unable to do 1 b) correctly, 
+#  you can run the following line to create the correct dat2 object
+#  (this will not earn you any marks for 1 b))
+load("test1objects.RData")
+
+
+# 1 d)
+
+
+# 1 e)
+# Note: In case you were unable to do 1 d) correctly, 
+#  you can run the following line to create the correct summary2 object
+#  (this will not earn you any marks for 1 d))
+load("test1objects.RData")
+
+
+# 1 f)
+
+
+# 1 g)
+# Note: In case you are unable to import the Excel file correctly, 
+#  you can run the following line to create the correct marriage_summary object
+#  (this will not earn you the marks for writing code to import the Excel file))
+load("test1objects.RData")
+
