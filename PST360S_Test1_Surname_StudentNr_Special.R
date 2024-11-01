adf104e8204dbfac9ff2ba6eb6b95337cd1c09a7
Thomas Joel Farrar <55871604+tjfarrar@users.noreply.github.com>
2024-11-01 08:48:10 +0200

    Add files via upload

---
+++ PST360S_Test1_Surname_StudentNr_Special.R
@@ -0,0 +1,48 @@
+# PST360S Test 1 Answer Script
+# Name: 
+# Student Number: 
+
+# Make sure you have installed the necessary packages:
+install.packages(c("tidyverse", "summarytools", "readxl"),
+                 dependencies = TRUE)
+
+# Answer each question under the corresponding code.
+# ONLY YOUR CODE WRITTEN IN THIS FILE WILL BE MARKED.
+
+# 1 a)
+
+
+# 1 b)
+# Note: In case you were unable to do 1 a) correctly, 
+#  you can run the following line to create the correct dat object
+#  (this will not earn you any marks for 1 a))
+load("test1specialobjects.RData")
+
+# 1 c)
+# Note: In case you were unable to do 1 b) correctly, 
+#  you can run the following line to create the correct dat2 object
+#  (this will not earn you any marks for 1 b))
+load("test1specialobjects.RData")
+
+
+# 1 d)
+
+
+# 1 e)
+# Note: In case you were unable to do 1 d) correctly, 
+#  you can run the following line to create the correct summary2 object
+#  (this will not earn you any marks for 1 d))
+load("test1specialobjects.RData")
+
+
+# 1 f)
+
+
+# 1 g)
+# Note: In case you are unable to import the Excel file correctly, 
+#  you can run the following line to create the correct cpi object
+# In case you are unable to do the join, you can use the joined_data 
+# object created by the below code for the linear regression part
+#  (this will not earn you the marks for writing code to import the Excel file))
+load("test1specialobjects.RData")
+
