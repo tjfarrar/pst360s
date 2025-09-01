5e4446825be8f7d888e8c5f19c06c60a662a36a3
Thomas Joel Farrar <55871604+tjfarrar@users.noreply.github.com>
2025-09-01 10:03:36 +0200

    Add files via upload

---
+++ PST360S_Test1_Surname_StudentNr_Special.R
@@ -0,0 +1,70 @@
+# Make sure you have installed the necessary packages:
+install.packages(c("tidyverse", "summarytools", "readxl", "broom"),
+                 dependencies = TRUE)
+
+# Answer each question under the corresponding comment, e.g. 1 a).
+# When finished, copy and paste everything into the Blackboard answer box.
+# Make sure to select all your code in the Blackboard answer box, 
+#  click the three dots, and click the Code snippet icon </> to 
+#  put your code into code snippet format. Then submit.
+
+# 1 a)
+
+
+# 1 b)
+# Note: In case you were unable to do 1 a) correctly, 
+#  you can run the following line to create the correct dat object
+#  (this will not earn you any marks for 1 a))
+load("test1specialobjects.RData")
+
+
+# 1 c)
+# Note: In case you were unable to do 1 b) correctly, 
+#  you can run the following line to create the correct dat2 object
+#  (this will not earn you any marks for 1 b))
+load("test1specialobjects.RData")
+
+
+# 1 d)
+# Note: In case you were unable to do 1 c) correctly, 
+#  you can run the following line to create the correct summary1 object
+#  (this will not earn you any marks for 1 c))
+load("test1specialobjects.RData")
+
+
+# 1 e)
+
+
+# 2 a)
+
+
+# 2 b)
+# Note: In case you were unable to do 2 a) correctly, 
+#  you can run the following line to create the correct 
+#   customers and transactions objects
+#  (this will not earn you any marks for 2 a))
+load("test1specialobjects.RData")
+
+
+# 2 c)
+# Note: In case you were unable to do 2 b) correctly, 
+#  you can run the following line to create the correct 
+#   pivoted_transactions object
+#  (this will not earn you any marks for 2 b))
+load("test1specialobjects.RData")
+
+
+# 2 d)
+# Note: In case you were unable to do 2 c) correctly, 
+#  you can run the following line to create the correct 
+#   joined_data object
+#  (this will not earn you any marks for 2 c))
+load("test1specialobjects.RData")
+
+
+
+# Remember: when finished, copy and paste everything into the Blackboard 
+#  answer box.
+# Make sure to select all your code in the Blackboard answer box, 
+#  click the three dots, and click the Code snippet icon </> to 
+#  put your code into code snippet format. Then submit.
