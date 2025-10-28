5aa164580cc13016fd200f7aa561a7ac51b0f8c9
Thomas Joel Farrar <55871604+tjfarrar@users.noreply.github.com>
2025-10-28 08:47:07 +0200

    Add files via upload

---
+++ PST360S_Test1_Special_Surname_StudentNr.R
@@ -0,0 +1,44 @@
+# PST360S Test 1 (Special Assessment) Answer Script
+# Name: 
+# Student Number: 
+
+# Make sure you have installed the necessary packages:
+install.packages(c("tidyverse", "summarytools", "readxl"),
+                 dependencies = TRUE)
+
+# Answer each question under the corresponding code.
+# ONLY CODE THAT IS COPIED-AND-PASTED TO BLACKBOARD WILL BE MARKED.
+
+# 1 a)
+
+
+# 1 b)
+# Note: In case you were unable to do 1 a) correctly, 
+#  you can run the following line to create the correct dat object
+#  (this will not earn you any marks for 1 a))
+load("test1specialobjects.RData")
+
+# 1 c)
+# Note: In case you were unable to do 1 b) correctly, 
+#  you can run the following line to create the correct dat2 object
+#  (this will not earn you any marks for 1 b))
+load("test1specialobjects.RData")
+
+
+# 1 d)
+
+
+# 1 e)
+# Note: In case you were unable to do 1 d) correctly, 
+#  you can run the following line to create the correct summary2 object
+#  (this will not earn you any marks for 1 d))
+load("test1specialobjects.RData")
+
+# 1 f)
+
+
+# 1 g)
+# Note: In case you are unable to import the Excel file correctly, 
+#  you can run the following line to create the correct cities object
+#  (this will not earn you the marks for writing code to import the Excel file))
+load("test1specialobjects.RData")
