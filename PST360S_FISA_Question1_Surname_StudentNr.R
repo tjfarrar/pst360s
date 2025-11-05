62e2cfe42941ce5b895cee11fccfbd0936f73bdd
Thomas Joel Farrar <55871604+tjfarrar@users.noreply.github.com>
2025-11-05 13:19:32 +0200

    Add files via upload

---
+++ PST360S_FISA_Question1_Surname_StudentNr.R
@@ -0,0 +1,57 @@
+# PST360S FISA Question 1 Answer Script
+# Name: 
+# Student Number: 
+
+# Make sure you have installed the necessary packages:
+install.packages(c("tidyverse", "summarytools", "readxl"),
+                 dependencies = TRUE)
+
+# Answer each question under the corresponding code.
+# ONLY CODE THAT IS PASTED INTO THE ANSWER BOX IN BLACKBOARD WILL BE MARKED.
+
+
+# 1 a) 
+
+
+# 1 b) 
+# Note: In case you were unable to do 1 a) correctly, 
+#  you can run the following line to create the correct dat object
+#  (this will not earn you any marks for 1 a))
+load("fisa_objects.RData")
+
+
+# 1 c) 
+# Note: In case you were unable to do 1 b) correctly, 
+#  you can run the following line to create the correct dat2 object
+#  (this will not earn you any marks for 1 b))
+load("fisa_objects.RData")
+
+
+# 1 d)
+# Note: In case you were unable to do 1 c) correctly, 
+#  you can run the following line to create the correct summary1 object
+#  (this will not earn you any marks for 1 c))
+load("fisa_objects.RData")
+
+
+# 1 e)
+# Note: In case you are unable to do the data cleaning part of the 
+# question, you can run the following line to create the correct 
+# dat3 object that you can use for the regression part.
+#  (this will not earn you any marks for creating dat3))
+load("fisa_objects.RData")
+
+
+# 1 f)
+# Note: In case you were unable to do 1 b) correctly, 
+#  you can run the following line to create the correct dat2 object
+#  (this will not earn you any marks for 1 b))
+load("fisa_objects.RData")
+
+
+# 1 g)
+# Note: In case you were unable to do 1 b) correctly, 
+#  you can run the following line to create the correct dat2 object
+#  (this will not earn you any marks for 1 b))
+load("fisa_objects.RData")
+
